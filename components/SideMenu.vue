a0c8a3de4face82a050e57a2a36a5c6b2dc90ec9
Ardec <Kamil.Kaczmarczyk.box@gmail.com>
2024-01-02 07:57:45 +0100

    my template is final version

---
+++ components/SideMenu.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="overlay-menu" v-if="menuVisible">
+    <!-- Tutaj dodaj zawartość menu, np. linki nawigacyjne -->
+    <nav>
+      <ul>
+        <li><a href="#home" @click="toggleMenu">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script setup>
+import { useGlobalState } from '~/composables/useGlobalState';
+const { menuVisible, toggleMenu } = useGlobalState();
+
+// Zmienna kontrolująca widoczność menu
+const isVisible = ref(true);
+
+// Tutaj możesz dodać logikę otwierania/zamykania menu
+</script>
+
+<style scoped>
+.overlay-menu {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 300px;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5); /* Półprzezroczyste tło */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+nav ul {
+  list-style: none;
+  padding: 0;
+}
+
+nav ul li a {
+  color: white;
+  text-decoration: none;
+  font-size: 20px;
+}
+</style>
---
+++ components/TopBar.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="topbar">
+   <div class="sub-title">Andorra</div>
+   <div class="title-container">
+    <div class="left-icon" @click="toggleMenu">menu {{menuVisible}}</div>
+    <div class="title">Guidedtour</div>
+    <div class="right-icon" @click="toggleMenu">close</div>
+   </div>
+   <div class="description">description</div>
+  </div>
+</template>
+
+<script setup>
+import { useGlobalState } from '~/composables/useGlobalState';
+const { menuVisible, toggleMenu } = useGlobalState();
+console.log('menuVisible.value in component:', menuVisible.value
+);
+</script>
+
+<style lang="scss" scoped>
+.topbar{
+    display:flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    min-width:300px;
+    padding:10px;
+    }
+    .sub-title {
+  color: #000;
+  text-align: center;
+  font-family: 'Poppins', sans-serif; // Użycie Poppins
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 700; // Waga 700 odpowiada wersji SemiBold
+  line-height: 29px; // 161.111%
+}
+    .title-container{
+        width:100%;
+        display:flex;
+        flex-direction: row;
+        justify-content: space-between;
+    }
+        .left-icon{
+            /* background-color: white; */
+            cursor:pointer;
+        }
+
+</style>
