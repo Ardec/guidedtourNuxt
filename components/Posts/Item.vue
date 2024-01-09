a324e5ff5951753cd0968a29948a77ac7943cf14
Justyna Kot <justyna.kot@euvi.pl>
2024-01-09 19:48:35 +0100

    Posts Components

---
+++ components/Posts/Item.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="wiz_box_container card_gradient">
+    <PostsPictograms :items="activeFlags"/>
+    <img
+      class="main-image"
+      :src="$attrs.item.image == null || $attrs.item.image == '' ? '/assets/project.jpeg' : baseUrl + $attrs.item.image"
+      onerror="this.onerror=null; this.src='assets/project.jpeg';" />
+      <PostsPictograms :items="activePictograms"/>
+    <PostsAddress :item="$attrs.item"/>
+  </div>
+</template>
+
+<script setup>
+const baseUrl = useBaseUrl();
+const attrs = useAttrs();
+const activeFlags = attrs.item?.flag?.map((item) => item.flag).filter((item) => item.isActive);
+const activePictograms = attrs.item?.pictograms?.map((item) => item.pictogram).filter((item) => item.isActive);
+</script>
+
+<style scoped lang="scss">
+.wiz_box_container {
+  width: 400px;
+  border-radius: 8px;
+  margin: 3px;
+  padding: 16px;
+
+  .main-image {
+    width: 150px;
+    height: 150px;
+    border-radius: 10px;
+  }
+}
+
+.card_gradient{
+  background:linear-gradient(135deg, #ffffff 0%,#dcdcdc 100%);
+  box-shadow: inset 0 2px 4px rgba(0,0,0,0.16), 0 2px 4px rgba(0,0,0,0.45);
+}
+.card_gradient:hover{
+  background:linear-gradient(135deg, #ffffff 0%,#e8e8e8 100%);
+  // opacity:0.6;
+}
+</style>
---
+++ components/Posts/Pictograms.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="pictograms">
+    <div v-for="(item, index) in $attrs.items" :key="item.id">
+      <div v-if="index < 9">
+        <!-- <div @click="change_location(item.flag.url)"> -->
+        <img :src="baseUrl + item.image" class="pictogram" />
+        <!-- <q-tooltip anchor="top middle" self="bottom middle" :offset="[10, 10]">
+            <strong>{{ item.flag.name }}</strong>
+            <em> {{ item.flag.url }}</em>
+          </q-tooltip> -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const baseUrl = useBaseUrl();
+</script>
+
+<style scoped lang="scss">
+.pictograms {
+  display: flex;
+}
+.pictogram {
+  height: 31px;
+  width: 31px;
+  border-radius: 3px;
+  cursor: pointer;
+  margin: 4px 4px 0 0;
+}
+</style>
