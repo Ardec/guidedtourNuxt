9bcf88df633f418ca59aa7a4293750c9f1f46a7e
Justyna Kot <justyna.kot@euvi.pl>
2024-01-13 18:12:24 +0100

    remove comment

--- pages/language.vue
+++ pages/language.vue
@@ -1,23 +1,5 @@
 <template>
-  <!-- {{ selectedLang }}
-  <USelect v-model="selectedLang" :options="alllang" option-attribute="name" value-attribute="id" /> -->
   <ChangeLanguage />
 </template>
 <script setup>
-// const lang = await useFetchLang();
-// const alllang = await useFetchAllLang();
-// const selectedLang = ref(alllang[0].id);
-// console.log(lang);
-// console.log(alllang);
-
-// watch(selectedLang, (newVal, oldVal) => {
-//   let newLang = alllang.find((item) => {
-//     console.log(item.id);
-
-//     return item.id == selectedLang.value;
-//   });
-//   if (newLang) {
-//     localStorage.setItem('lang', JSON.stringify(newLang));
-//   }
-// });
 </script>
