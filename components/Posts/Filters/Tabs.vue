fe16ec3be799b32fe44b19363134b01831128e78
Justyna Kot <justyna.kot@euvi.pl>
2024-01-25 22:23:02 +0100

    Filters by tags

--- components/Posts/Filters/Tabs.vue
+++ components/Posts/Filters/Tabs.vue
@@ -6,25 +6,38 @@
         :filters="$attrs.filters"
         :lang="$attrs.lang"
         :isTimeFilterActive="$attrs.isTimeFilterActive"
-        :isEventFilterActive="$attrs.isEventFilterActive"
-        :isStartEndFilterActive="$attrs.isStartEndFilterActive" />
-      <PostsFiltersTags v-if="item.key === 'tags'" :tags="$attrs.tags" :lang="$attrs.lang" />
+        :isEventFilterActive="$attrs.isEventFilterActive" />
+      <PostsFiltersTags
+        v-if="item.key === 'tags'"
+        :localisationTags="$attrs.localisationTags"
+        :restOfTags="$attrs.restOfTags"
+        :lang="$attrs.lang" />
     </template>
   </UTabs>
 </template>
 <script setup>
+const attrs = useAttrs();
+const localisationTags = attrs.localisationTags;
+const restOfTags = attrs.restOfTags;
+
 const tabsItems = [
   {
     label: 'Basic',
     key: 'basic',
   },
-  {
+];
+
+if (localisationTags?.length > 0 || restOfTags?.length > 0) {
+  tabsItems.push({
     label: 'Tags',
     key: 'tags',
-  },
-  {
+  });
+}
+
+if (false) {
+  tabsItems.push({
     label: 'Map',
     key: 'map',
-  },
-];
+  });
+}
 </script>
